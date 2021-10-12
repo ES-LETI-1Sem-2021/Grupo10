51d4783d94ee372c5dcfd7a9c0428138a0c9ff80
Olek <roguezilla0@gmail.com>
2021-10-12 21:16:25 +0100

    ficheiros de teste

---
+++ src/test/java/pt/iscte/iul/Main.java
@@ -0,0 +1,12 @@
+package pt.iscte.iul;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Main {
+    @Test
+    public void stringTest() {
+        String test = "1";
+        assertEquals("1", test);
+    }
+}

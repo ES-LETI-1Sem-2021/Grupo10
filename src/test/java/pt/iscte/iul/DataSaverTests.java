583a6cc90dcf73c035def1635d4ddf249aca81cd
rfgoo <rfgoo@iscte-iul.pt>
2021-10-27 20:12:13 +0100

    Adicionados teste unitários para a classe DataSaver.

--- src/test/java/pt/iscte/iul/DataSaverTests.java
+++ src/test/java/pt/iscte/iul/DataSaverTests.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 public class DataSaverTests {
+
     @Test
     public void TestSave(){
+
         String[] str = {"str_line1","str_line2","str_line3"};
         DataSaver.save(str,str,"test_files/data_saver_tests.txt");
 

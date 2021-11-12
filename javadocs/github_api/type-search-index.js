73b81bb0e41865ab5e6d51ab3ce3e284821ffed1
Olek <roguezilla0@gmail.com>
2021-11-12 10:33:40 +0000

    Re-generate javadocs.

--- javadocs/github_api/type-search-index.js
+++ javadocs/github_api/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"pt.iscte.iul","l":"GitHubAPI.Collaborators"},{"p":"pt.iscte.iul","l":"GitHubAPI.Date"},{"p":"pt.iscte.iul","l":"GitHubAPI"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"pt.iscte.iul","l":"GitHubAPI.Branch"},{"p":"pt.iscte.iul","l":"GitHubAPI.Collaborators"},{"p":"pt.iscte.iul","l":"GitHubAPI.CommitData"},{"p":"pt.iscte.iul","l":"GitHubAPI.Commits"},{"p":"pt.iscte.iul","l":"GitHubAPI.Date"},{"p":"pt.iscte.iul","l":"GitHubAPI"}];updateSearchResults();

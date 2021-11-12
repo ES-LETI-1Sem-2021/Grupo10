3c40f35dac674df259d82fdd82ef120a385fe063
Olek <roguezilla0@gmail.com>
2021-11-12 10:11:56 +0000

    Add getCommits, getBranches, fix&add new/better javadocs.

--- src/test/java/pt/iscte/iul/GitHubAPITests.java
+++ src/test/java/pt/iscte/iul/GitHubAPITests.java
@@ -22,13 +22,13 @@ public class GitHubAPITests {
 
     @Test
     public void numberOfCollaborators() throws IOException {
-        var c = api.getCollaborators();
+        var collaborators = api.getCollaborators();
 
-        Assertions.assertEquals(4, c.length);
+        Assertions.assertEquals(5, collaborators.length);
 
-        var names = new String[] {"Olek", "Duarte", "Miguel", "Rodrigo Guerreiro"};
-        for (int i = 0; i < c.length; i++) {
-            Assertions.assertEquals(names[i], c[i].getName());
+        var names = new String[] {"Olek", null, "Duarte", "Miguel", "Rodrigo Guerreiro"};
+        for (int i = 0; i < collaborators.length; i++) {
+            Assertions.assertEquals(names[i], collaborators[i].getName());
         }
     }
 
@@ -37,13 +37,20 @@ public class GitHubAPITests {
         // may be changed in the future
         String readme = """
                 # Aplicação para monitorização e avaliação de progresso de projetos de software que seguem a abordagem SCRUM
-                                
                 ------------------------------------------
                 # Projeto desenvolvido por:
-                  - Duarte Casaleiro, nº 92697
-                  - Miguel Romana, nº 92688
-                  - Oleksandr Kobelyuk, nº 92402
-                  - Rodrigo Guerreiro, nº 92388
+                - Duarte Casaleiro, nº 92697
+                - Miguel Romana, nº 92688
+                - Oleksandr Kobelyuk, nº 92402
+                - Rodrigo Guerreiro, nº 92388
+                                
+                ## Sprint 1:
+                ### Este Sprint teve como resultados:
+                - Uma GUI funcional, onde é possivel observar:
+                > - O ficheiro ([README.md](https://github.com/Roguezilla/ES-LETI-1Sem-2021-Grupo10#readme))
+                > - Os colaboradores (acedendo ainda às suas páginas do GitHub)
+                > - Nome do projeto e sua data de início
+                - Datas de ínicio e fim dos sprints
                 """;
         Assertions.assertEquals(readme, this.api.getFile("master","/README.md"));
     }
@@ -56,9 +63,36 @@ public class GitHubAPITests {
     @Test
     public void dateTest() throws IOException {
         var date = this.api.getStartTime();
-        Assertions.assertEquals("2021-10-08", date.getFormatted());
+        Assertions.assertEquals("2021-10-08", date.toString());
         Assertions.assertEquals("2021", date.getYear());
         Assertions.assertEquals("10", date.getMonth());
         Assertions.assertEquals("08", date.getDay());
+    }
+
+    @Test
+    public void branchTest() throws IOException {
+        var branches = api.getBranches();
+
+        Assertions.assertEquals(4, branches.length);
+
+        var names = new String[] {"github_api", "master", "trello_api", "ui_test"};
+        for (int i = 0; i < branches.length; i++) {
+            Assertions.assertEquals(names[i], branches[i].getName());
+        }
+    }
+
+    @Test
+    public void commitTest() throws IOException {
+        var commits = api.getCommits("ui_test", "Roguezilla");
+
+        System.out.println("Committer: " + commits.getCommitter());
+        System.out.println("Numer of commits: " + commits.getCommitData().size());
+        System.out.println("First commit: " + commits.getCommitData().get(0).getMessage());
+        System.out.println("First commit date: " + commits.getCommitData().get(0).getDate());
+        System.out.println("Lastest commit: " + commits.getCommitData().get(commits.getCommitData().size()-1).getMessage());
+        System.out.println("Lastest commit date: " + commits.getCommitData().get(commits.getCommitData().size()-1).getDate());
+
+        Assertions.assertNotEquals(0, commits.getCommitData().size());
+        commits.getCommitData().forEach(commitData -> Assertions.assertNotEquals(null, commitData.getMessage()));
     }
 }

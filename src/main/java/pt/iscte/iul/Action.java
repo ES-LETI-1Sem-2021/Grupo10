3392bd1205d78bfabbef25baf63b3ea45c22336e
rfgoo <rfgoo@iscte-iul.pt>
2021-11-25 21:03:04 +0000

    inproved charts times

--- src/main/java/pt/iscte/iul/Action.java
+++ src/main/java/pt/iscte/iul/Action.java
@@ -134,63 +134,14 @@ public class Action {
         editorPane.setVisible(true);
         editorPane.setBounds(100, 100, 500, 600);
 
-        //addTotalHoursInfo(frame, boardID, trelloAPI);
-
         frame.add(editorPane);
         frame.setVisible(true);
+
+        addHoursInfo(frame,"",boardID,trelloAPI);
+        frame.repaint();
+        frame.validate();
     }
 
-    private static void addTotalHoursInfo(JFrame frame, String boardID, TrelloAPI trelloAPI) throws IOException{
-        ArrayList<TrelloAPI.HoursPerUser> hoursPerUsers = trelloAPI.getTotalHoursByUser(boardID,"","");
-
-        DefaultPieDataset<String> dataset= new DefaultPieDataset<>();
-        DefaultPieDataset<String> spentHoursDataset= new DefaultPieDataset<>();
-
-        hoursPerUsers.forEach(e->{
-            dataset.setValue(e.getUser(),e.getEstimatedHours());
-            spentHoursDataset.setValue(e.getUser(),e.getEstimatedHours());
-        });
-        JFreeChart chart = ChartFactory.createPieChart(
-                "Hours Estimated by user for the project." ,
-                dataset, true,true,false);
-
-        JFreeChart spentHoursChart = ChartFactory.createPieChart(
-                "Hours Spent by user for the project.",
-                dataset, true,true,false);
-
-        ChartPanel cp = new ChartPanel(chart);
-        cp.setBounds(750, 0, 300, 250);
-        cp.setVisible(true);
-
-        ChartPanel spentCP = new ChartPanel(spentHoursChart);
-        spentCP.setBounds(1100, 0, 300, 250);
-        spentCP.setVisible(true);
-
-        frame.add(cp);
-        frame.add(spentCP);
-        frame.setVisible(true);
-        String[][] data = new String[hoursPerUsers.size()+1][3];
-        String[] names = {"User", "Estimated Hours","Spent Hours"};
-
-        data[0] = names;
-
-        for(int cont = 0; cont!=hoursPerUsers.size();cont++){
-            data[cont+1] = new String[]{hoursPerUsers.get(cont).getUser(),
-                    String.valueOf(hoursPerUsers.get(cont).getEstimatedHours()),
-                    String.valueOf(hoursPerUsers.get(cont).getSpentHours())};
-        }
-
-        JTable table = new JTable(data, names);
-        table.setBounds(750,300,400,250);
-        table.setVisible(true);
-        table.setEnabled(false);
-        table.setGridColor(Color.BLACK);
-        table.setShowGrid(true);
-
-        frame.add(table);
-        frame.setVisible(true);
-
-    }
 
     /**
      * Function that adds a pie chart to the ui as well as a table with the same information.
@@ -215,11 +166,11 @@ public class Action {
             spentHoursDataset.setValue(e.getUser(),e.getEstimatedHours());
         });
         JFreeChart chart = ChartFactory.createPieChart(
-                "Hours Estimated by user for the " + sprintName,
+                "Hours Estimated by user " + sprintName,
                 dataset, true,true,false);
 
         JFreeChart spentHoursChart = ChartFactory.createPieChart(
-                "Hours Spent by user for the " + sprintName,
+                "Hours Spent by user " + sprintName,
                 dataset, true,true,false);
 
         ChartPanel cp = new ChartPanel(chart);
